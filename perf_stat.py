a9ae3bf18c7566c1c4ae571f24b953ba1cf3f1a0
yamabato <harutakutsukawa0601@gmail.com>
2024-07-18 19:04:38 +0900

    最良のパフォーマンスを記録

--- perf_stat.py
+++ perf_stat.py
@@ -3,7 +3,7 @@ import csv
 import subprocess
 
 MAX_DATA_NUM = 15
-COUNT = 5
+COUNT = 100
 
 perf = []
 eff_ave = []
@@ -15,10 +15,11 @@ for i in range(MAX_DATA_NUM+1):
         output = subprocess.run(f"./tsp -p -d {i}", shell=True, capture_output=True, text=True).stdout
         perf[-1].append(float(output.split("\n")[1]))
 
-    eff_ave.append(round(sum(perf[-1])/COUNT, 2))
+    # eff_ave.append(round(sum(perf[-1])/COUNT, 2))
+    eff_ave.append(round(min(perf[-1]), 2))
 
 print(eff_ave)
 
-with open("perf.csv", "a") as f:
+with open("perf_best.csv", "a") as f:
     writer = csv.writer(f, lineterminator="\n")
     writer.writerow([sys.argv[1]] + [f"{e:.2f}" for e in eff_ave])
